5834112bfa139d2112f27af67d8f7c98382ff98c
Dyuti Ballav Paul <112813661+Dyuti01@users.noreply.github.com>
2024-06-16 22:50:26 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -16,14 +16,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* <div className="fixed top-0 z-[-2] h-screen w-screen dark:bg-[#000000] dark:bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]">
-          <div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-blue-500 opacity-50 blur-[100px]"></div>
-          </div> */}
+        <div className="fixed inset-0 -z-10 h-full w-screen dark:bg-slate-950 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
+        </div>
 
-        <div className="fixed inset-0 -z-10 h-full w-screen dark:bg-slate-950 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"><div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full dark:bg-violet-500 opacity-40 blur-[100px]"></div></div>
-
+        <div className="absolute inset-0 -z-10 h-full w-full dark:bg-slate-800 [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#63e_100%)] dark:[background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+    
         <UserDataProvider>
-
           <Navbar />
           <div className="min-h-[700px] overflow-x-hidden">
             {children}
