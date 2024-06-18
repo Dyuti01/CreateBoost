742c6245e2abc52db2c1f994bbd636edc9b262a1
Dyuti Ballav Paul <112813661+Dyuti01@users.noreply.github.com>
2024-06-18 11:25:20 +0530

    Update SearchResults.jsx

--- components/SearchResults.jsx
+++ components/SearchResults.jsx
@@ -21,7 +21,7 @@ const SearchResults = ({query}) => {
     <ul>
       {users.map((creator)=>{
         if (creator.name.toLowerCase().includes(query)){
-          return <li className='py-3 px-4 rounded-xl hover:bg-slate-700'><Link href={`${process.env.NEXT_PUBLIC_URL}/profile/${creator.username}`}>
+          return <li className='py-3 px-4 rounded-xl text-slate-500 hover:text-slate-50 hover:bg-slate-700'><Link href={`${process.env.NEXT_PUBLIC_URL}/profile/${creator.username}`}>
             {creator.name}
             </Link></li>}
       })}
